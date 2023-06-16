7e7d1d9f5cd6b533a5c248dd40b35aae23856ef0
velvet <ivan.guard1n@gmail.com>
2023-06-16 19:28:30 +0300

    #

---
+++ sec/send_email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Получение данных из формы
+    $full_name = $_POST['full-name'];
+    $city = $_POST['city'];
+    $contact_info = $_POST['contact-info'];
+
+    // Формирование сообщения
+    $message = "Новое сообщение с формы обратной связи:\n\n";
+    $message .= "Имя: " . $full_name . "\n";
+    $message .= "Город: " . $city . "\n";
+    $message .= "Контакт: " . $contact_info . "\n";
+
+    // Отправка письма
+    $to = "sobaka.pisos2123@gmail.com";
+    $subject = "Новое сообщение с формы обратной связи";
+    $headers = "From: Your Website <noreply@yourwebsite.com>";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Сообщение успешно отправлено!";
+    } else {
+        echo "Ошибка при отправке сообщения.";
+    }
+}
+?>
